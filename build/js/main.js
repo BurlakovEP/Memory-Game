650ba642a8022023a33edf0c24a748265fc32fb2
unknown <burlakov_e_p@mail.ru>
2019-09-04 12:58:30 +0500

    Added animation, fixed some bugs

--- build/js/main.js
+++ build/js/main.js
@@ -1,103 +1,80 @@
-"use strict";
+window.onload = () => {
+  const getElem = (selector) => document.querySelector(`.${selector}`);
+  const getRandom = () => Math.random() - 0.5;
+  const startPage = getElem('start-page');
+  const gamePage = getElem('game-page');
+  const endPage = getElem('end-page');
+  const scoreField = getElem('game-page__field');
+  const cards = document.querySelectorAll('.card');
 
-window.onload = function () {
-  var getElem = function getElem(selector) {
-    return document.querySelector('.' + selector);
-  };
+  function changeState(elem, param) {
+    const { classList } = elem;
+    let newtState;
 
-  var getRandom = function getRandom(a, b) {
-    return Math.random() - 0.5;
-  };
+    if (param === 'show') newtState = '_visible';
+    if (param === 'hide') newtState = '_invisible';
+    if (param === 'flip') newtState = '_flipped';
 
-  var startPage = getElem('start-page');
-  var gamePage = getElem('game-page');
-  var endPage = getElem('end-page');
-  var gameDesk = getElem('game-page__main');
-  var deck = ['2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '0C', '0D', '0H', '0S', 'AC', 'AD', 'AH', 'AS', 'JC', 'JD', 'JH', 'JS', 'KC', 'KD', 'KH', 'KS', 'QC', 'QD', 'QH', 'QS'];
-  var clickCount = 0;
-  var cardOne = '';
-  var cardTwo = '';
-  var cardOneId = '';
-  var cardTwoId = '';
-  var openPair = 0;
-  var closePair = 9;
-  var score = 0;
-  var scoreField = getElem('game-page__field');
+    classList.remove(classList[classList.length - 1]);
+    classList.add(classList[0] + newtState);
+  }
+
+  function setPath(elem, param) {
+    elem.setAttribute('src', param);
+  }
+
+  const deck = [
+    '2C', '2D', '2H', '2S', '3C', '3D', '3H', '3S', '4C', '4D', '4H', '4S', '5C', '5D', '5H', '5S', '6C', '6D', '6H', '6S', '7C', '7D', '7H', '7S', '8C', '8D', '8H', '8S', '9C', '9D', '9H', '9S', '0C', '0D', '0H', '0S', 'AC', 'AD', 'AH', 'AS', 'JC', 'JD', 'JH', 'JS', 'KC', 'KD', 'KH', 'KS', 'QC', 'QD', 'QH', 'QS',
+  ];
+
+  let clickCount = 0;
+  let cardOne = '';
+  let cardTwo = '';
+  let cardOneKey = '';
+  let cardTwoKey = '';
+  let cardOneId = '';
+  let cardTwoId = '';
+  let openPair = 0;
+  let closePair = 9;
+  let score = 0;
+
   scoreField.innerHTML = score;
 
-  getElem('start-page__button').onclick = function () {
-    startPage.classList.toggle('page_state_visible', false);
-    startPage.classList.toggle('page_state_invisible', true);
-    gamePage.classList.toggle('page_state_invisible', false);
-    gamePage.classList.toggle('page_state_visible', true);
-    createGameDesk();
-  };
-
-  getElem('game-page__button').onclick = function () {
-    gameDesk.innerHTML = '';
-    resetCards();
-  };
-
-  getElem('end-page__button').onclick = function () {
-    endPage.classList.toggle('page_state_visible', false);
-    endPage.classList.toggle('page_state_invisible', true);
-    gamePage.classList.toggle('page_state_invisible', false);
-    gamePage.classList.toggle('page_state_visible', true);
-    resetCards();
-  };
-
-  function createGameDesk() {
-    setTimeout(flipAllCards, 5000);
-    deck.sort(getRandom);
-    var sliceDeck = deck.slice(0, 9);
-    var newDeck = sliceDeck.concat(sliceDeck);
-    var newDeckSort = newDeck.sort(getRandom);
-
-    for (var i = 0; i < newDeckSort.length; i++) {
-      var card = document.createElement("img");
-      card.className = "game-page__image";
-      card.setAttribute("id", i + 1);
-      card.setAttribute("src", "img/" + newDeckSort[i] + ".png");
-      card.setAttribute("data-key", newDeckSort[i]);
-      gameDesk.append(card);
-    }
-  }
-
-  function flipAllCards() {
-    var cards = gamePage.getElementsByTagName("img");
-
-    for (var i = 0; i < cards.length; i++) {
-      cards[i].setAttribute("src", "img/BG.png");
-      cards[i].onclick = getCard;
-    }
-  }
-
   function getCard() {
-    clickCount++;
-    var cardId = this.getAttribute("id");
-    var cardItem = document.getElementById(cardId);
-    var key = cardItem.getAttribute("data-key");
-    var srcItem = "img/" + key + ".png";
+    clickCount += 1;
+
+    const id = this.getAttribute('id');
+    const key = this.getAttribute('data-key');
+    const src = `img/${key}.png`;
 
     if (clickCount === 1) {
-      cardOne = key;
-      cardOneId = cardId;
-      cardItem.setAttribute("src", srcItem);
+      cardOne = this;
+      cardOneKey = key;
+      cardOneId = id;
+      changeState(cardOne, 'show');
+      setTimeout(setPath, 300, cardOne, src);
     } else if (clickCount === 2) {
-      cardTwo = key;
-      cardTwoId = cardId;
-      cardItem.setAttribute("src", srcItem);
+      cardTwo = this;
+      cardTwoKey = key;
+      cardTwoId = id;
+      changeState(cardTwo, 'show');
+      setTimeout(setPath, 300, cardTwo, src);
 
-      if (cardOne === cardTwo && cardOneId !== cardTwoId) {
-        var hideCards = function hideCards() {
-          document.getElementById(cardOneId).style.visibility = "hidden";
-          document.getElementById(cardTwoId).style.visibility = "hidden";
+      if (cardOneKey === cardTwoKey && cardOneId !== cardTwoId) {
+        const hideCards = () => {
+          changeState(cardOne, 'hide');
+          changeState(cardTwo, 'hide');
+          cardOne.removeAttribute('src');
+          cardTwo.removeAttribute('src');
+          cardOne.onclick = false;
+          cardTwo.onclick = false;
+
           clickCount = 0;
         };
+        setTimeout(hideCards, 1000);
 
-        setTimeout(hideCards, 600);
-        closePair--;
-        openPair++;
+        closePair -= 1;
+        openPair += 1;
 
         if (closePair === 0) {
           score += 42;
@@ -107,15 +84,17 @@ window.onload = function () {
 
         scoreField.innerHTML = score;
       } else {
-        var flipCards = function flipCards() {
-          document.getElementById(cardOneId).src = "img/BG.png";
-          document.getElementById(cardTwoId).src = "img/BG.png";
+        const flipCards = () => {
+          changeState(cardOne, 'flip');
+          changeState(cardTwo, 'flip');
+          setTimeout(setPath, 200, cardOne, 'img/BG.png');
+          setTimeout(setPath, 200, cardTwo, 'img/BG.png');
+
           clickCount = 0;
         };
-
         setTimeout(flipCards, 1000);
-        score -= openPair * 42;
 
+        score -= openPair * 42;
         if (score <= 0) {
           score = 0;
         }
@@ -125,17 +104,36 @@ window.onload = function () {
     }
 
     if (closePair === 0 && openPair === 9) {
-      var goToEndPage = function goToEndPage() {
-        gameDesk.innerHTML = '';
-        gamePage.classList.toggle('page_state_visible', false);
-        gamePage.classList.toggle('page_state_invisible', true);
-        endPage.classList.toggle('page_state_invisible', false);
-        endPage.classList.toggle('page_state_visible', true);
-        getElem("end-page__field").innerHTML = score;
+      const goToEndPage = () => {
+        changeState(gamePage, 'hide');
+        changeState(endPage, 'show');
+        getElem('end-page__field').innerHTML = score;
       };
-
-      setTimeout(goToEndPage, 1000);
+      setTimeout(goToEndPage, 1100);
     }
+  }
+
+  function flipAllCards() {
+    for (let i = 0; i < cards.length; i++) {
+      cards[i].classList.add('card_flipped');
+      setTimeout(setPath, 200, cards[i], 'img/BG.png');
+      cards[i].onclick = getCard;
+    }
+  }
+
+  function createGameDesk(delay) {
+    deck.sort(getRandom);
+    const sliceDeck = deck.slice(0, 9);
+    const newDeck = sliceDeck.concat(sliceDeck);
+    const newDeckSort = newDeck.sort(getRandom);
+
+    for (let i = 0; i < newDeckSort.length; i++) {
+      cards[i].className = 'card';
+      setTimeout(setPath, delay, cards[i], `img/${newDeckSort[i]}.png`);
+      cards[i].setAttribute('id', i + 1);
+      cards[i].setAttribute('data-key', newDeckSort[i]);
+    }
+    setTimeout(flipAllCards, 5000);
   }
 
   function resetCards() {
@@ -143,6 +141,22 @@ window.onload = function () {
     closePair = 9;
     score = 0;
     scoreField.innerHTML = score;
-    createGameDesk();
+    createGameDesk(0);
   }
+
+  getElem('start-page__button').onclick = () => {
+    changeState(startPage, 'hide');
+    changeState(gamePage, 'show');
+    createGameDesk(0);
+  };
+
+  getElem('game-page__button').onclick = () => {
+    resetCards();
+  };
+
+  getElem('end-page__button').onclick = () => {
+    changeState(endPage, 'hide');
+    changeState(gamePage, 'show');
+    resetCards();
+  };
 };
